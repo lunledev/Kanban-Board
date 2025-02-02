cb98003423dbf3d16914d73410d862d8bccade58
lunledev <183235731+lunledev@users.noreply.github.com>
2025-02-02 11:27:49 -0800

    added query for one record and check if user  authentication failed

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -7,6 +7,30 @@ export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
   
   const {username, password} = req.body; //getting data from client username and podsword.
+
+  //variable user to store result of query
+  const user = await User.findOne ({ //waits for query to complete for User(referening users table) and returns the results. 
+  // User- refer to User table , findOne - ORM get a single record from database that match criteria where.
+
+  where: {username}, //query users table where username matches variable username.
+  
+  });
+
+  //if user does not exist return 401(unauthorized) 
+  // and a message of authentification failed.
+  if(!user)
+  {
+    return res.status(401).json({message: 'Authentication failed'});
+  }
+
+  //checks if passward is valid by ussing bcrypt by comparing password entered
+  // with the stored user password in a database.
+  
+
+
+
+
+
   
 
 
